1d3ce139ce06ab497323174f4e8f80e255a67c2c
leeshanex <leeshanex@outlook.com>
2020-05-21 04:15:16 -0700

    scaffolded identity

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TrashCollector_Proj.Data;
+
+[assembly: HostingStartup(typeof(TrashCollector_Proj.Areas.Identity.IdentityHostingStartup))]
+namespace TrashCollector_Proj.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
