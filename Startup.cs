abc496be752802d007c19f4d1723a4a3f8414db8
leeshanex <leeshanex@outlook.com>
2020-05-21 05:23:01 -0700

    added global routing

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,9 @@ using TrashCollector_Proj.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using TrashCollector_Proj.ActionFilters;
 
 namespace TrashCollector_Proj
 {
@@ -34,6 +37,8 @@ namespace TrashCollector_Proj
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultUI()
                 .AddDefaultTokenProviders();
+            services.AddScoped<ClaimsPrincipal>(s => s.GetService<IHttpContextAccessor>().HttpContext.User);
+            services.AddControllers(config => { config.Filters.Add(typeof(GlobalRouting)); });
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
