3b0951f850a205cedc0f736fca2a93681d4b803d
leeshanex <leeshanex@outlook.com>
2020-05-22 04:00:30 -0700

    New customer class properties

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -13,6 +13,13 @@ namespace TrashCollector_Proj.Models
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public int Balance { get; set; }
+        public string DayOfWeekPickUp { get; set; }
+
 
         [ForeignKey("IdentityUser")]
         public string IdentityUserId { get; set; }
